ad81a5eaf6028def2c35f11d3a77d214db4fd7b0
Joanne Lee <vernapii@gmail.com>
2018-03-13 20:29:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(2,3)
+  return array.slice(2)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
