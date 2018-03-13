dc0d3152d2fbc40d8df835e3fa711dd9a3b101a7
Joanne Lee <vernapii@gmail.com>
2018-03-13 20:30:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
-  array
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
